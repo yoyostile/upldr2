a7c864bf9ea71005172d5290847dc7a13dc6af45
Johannes Heck <yoyostile@gmail.com>
2017-01-17 09:34:41 +0100

    fix large gif uploading

--- app/uploaders/file_uploader.rb
+++ app/uploaders/file_uploader.rb
@@ -8,15 +8,34 @@ class FileUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def only_first_frame
+    manipulate! do |img|
+      if img.mime_type.match /gif/
+        if img.scene == 0
+          img = img.cur_image #Magick::ImageList.new( img.base_filename )[0]
+        else
+          img = nil # avoid concat all frames
+        end
+      end
+      img
+    end
+  end
+
   version :large, if: :image? do
+    process :only_first_frame
+    process convert: 'jpg'
     process resize_to_fit: [1800, 1800]
   end
 
   version :medium, if: :image? do
+    process :only_first_frame
+    process convert: 'jpg'
     process resize_to_fit: [900, 900]
   end
 
   version :thumb, if: :image? do
+    process :only_first_frame
+    process convert: 'jpg'
     process resize_to_fill: [250, 250]
   end
   protected
